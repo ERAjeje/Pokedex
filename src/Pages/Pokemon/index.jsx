12d5910981fca3297603cdfbd32d6d9d4a731b28
Edson <edson.ajeje@terra.com.br>
2022-03-07 19:09:35 -0300

    inclusao de visualizacao de dados do pokemon

--- src/Pages/Pokemon/index.jsx
+++ src/Pages/Pokemon/index.jsx
@@ -1,9 +1,15 @@
+import styled from "styled-components";
+
+import Row from "../../components/Row";
+import Column from "../../components/Column"
+import UtilArea from "../../components/UtilArea";
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import { getItem } from "../../api/api";
-import Row from "../../components/Row";
-import UtilArea from "../../components/UtilArea";
+import Chart from "../../components/Chart";
+import TextSpan from "../../components/TextSpan";
+import EvolutionChain from "../../components/EvolutionChain";
 
 const CardPokemon = styled.div`
     display: flex;
@@ -13,24 +19,58 @@ const CardPokemon = styled.div`
 `;
 
 const Sprite = styled.img`
+    margin: 15px 5px;
+    width: ${props => props.width ?? '255px'};
 `;
 
 export default () => {
     const [pokemon, setPokemon] = useState(null);
+    const [evolution, setEvolution] = useState(null);
     const { id } = useParams();
 
     useEffect(() => {
         const asyncFunction = async () => {
-            const data = await getItem(id);
+            let data = await getItem(`pokemon/${id}`);
             setPokemon({ ...data });
+            data = await getItem(`evolution-chain/${id}/`);
+            setEvolution({ ...data })
         }
         asyncFunction();
     }, [id])
     return <UtilArea>
-        {pokemon !== null ?
+        {pokemon !== null && evolution !== null ?
             <CardPokemon>
-                <Row justify='start' height='40%' >
-                    <Sprite src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.forms[0].name} />
+                <Row justify='start' >
+                    <Sprite src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
+                    <Column>
+                        <TextSpan capitalize size='2.5rem'>{`Nº ${pokemon.order} - ${pokemon.name}`}</TextSpan>
+                        <Row>
+                            <TextSpan weight='light' >{`Height: ${pokemon.height}`}</TextSpan>
+                            <TextSpan weight='light' >{`Weight: ${pokemon.weight}`}</TextSpan>
+                        </Row>
+                        <Row>
+                            {
+                                pokemon.abilities.map((item, index) =>
+                                    <TextSpan key={index} weight='light' >{`${item.ability.name}${index % 2 === 0 ? ' /' : ''}`}</TextSpan>)
+                            }
+                        </Row>
+                        <Row>
+                            <TextSpan weight='light' >Type: </TextSpan>
+                            {
+                                pokemon.types.map((item, index) =>
+                                    <TextSpan key={index} weight='light'>{`${item.type.name}${index % 2 === 0 ? ' /' : ''}`}</TextSpan>
+                                )
+                            }
+                        </Row>
+                    </Column>
+                </Row>
+                <Row>
+                    <Column>
+                        <Chart data={pokemon.stats} size={450} />
+                    </Column>
+                    <Column>
+                        <EvolutionChain data={evolution} />
+                    </Column>
                 </Row>
             </CardPokemon>
             : ''
