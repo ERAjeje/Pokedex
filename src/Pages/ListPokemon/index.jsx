0dc3701e463b107cb4732bedc7b327adaf538991
Edson <edson.ajeje@terra.com.br>
2022-03-07 15:19:06 -0300

    correção de func async em useeffect (doc)

--- src/Pages/ListPokemon/index.jsx
+++ src/Pages/ListPokemon/index.jsx
@@ -9,18 +9,17 @@ const aux = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 export default function Display({ offset }) {
     const [pokemon, setPokemon] = useState([]);
 
-    const _getItens = async () => {
-        const data = await getList(offset);
-        setPokemon(data.results);
-    }
-
     useEffect(() => {
-        _getItens();
+        const asyncFunction = async () => {
+            const data = await getList(offset)
+            setPokemon(data.results)
+        }
+        asyncFunction()
     }, [offset])
 
     return <UtilArea>
         {
-            pokemon.map((item, index) => <Link key={item.name} to={`/${index + 1}`}><Card index={index + 1 + offset} name={item.name} /></Link>)
+            pokemon.map((item, index) => <Link key={item.name} to={`/${index + 1 + offset}`}><Card index={index + 1 + offset} name={item.name} /></Link>)
         }
     </UtilArea>
 }
