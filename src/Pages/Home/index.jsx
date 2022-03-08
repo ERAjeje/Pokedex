613cc3feb15e26120bde4f82ced6b364506196d9
Edson <edson.ajeje@terra.com.br>
2022-03-08 18:52:11 -0300

    retirada de styled Button

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -14,36 +14,12 @@ const Background = styled.div`
     height: 100vh;
 `;
 
-const Button = styled.button`
-    background-color: #1394F0;
-    color: white;
-    width: 150px;
-    height: 150px;
-    border-radius: 80px;
-    font-size: 90px;
-    font-weight: bold;
-    border: none;
-    margin: auto;
-    box-shadow: 1px 2px 5px rgba(0, 0, 0, 1);
-    @media screen and (min-width: 768px) and (max-width: 1023px) {
-        width: 120px;
-        height: 120px;
-        border-radius: 60px;
-        margin: 0 25px;
-    }
-    @media(max-width: 767px) {
-        display: none;
-    }
-`;
-
-
-
 export default function Home() {
 
     return (
         <Background>
             <Row>
-                <Heather />
+                <Heather data-testid='heather' />
             </Row>
             <Row>
                 <Router>
